4efc69cc8939560e4615252e74928bd3eb50b34f
Seonghoon (Martin) Park <martin941079@gmail.com>
2023-11-05 22:23:23 +0900

    Got back into the game; had to catch up with how to start the service overall, and left some notes

---
+++ react_frontend/tapa_frontend/src/Home.tsx
@@ -0,0 +1,20 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <ul className="home-components">
+          <li className="home-components"></li>
+          <li className="home-components"></li>
+          <li className="home-components"></li>
+          <li className="home-components"></li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+// {/* Logo */}
+// {/* Username Text box */}
+// {/* Password Text box */}
+// {/* Log in Button */}
+// {/* Sign Up Page button */}
